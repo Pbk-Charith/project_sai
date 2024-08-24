1d2b827206bdef245e236b003a51998386268d8c
charith <pbcharith01@gmail.com>
2024-08-24 17:07:44 +0530

    Added Calculator

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from main import Calculator
+
+obj=Calculator()
+def test_add():
+    assert obj.add(1,2)==3
+
+def test_sub():
+    assert obj.sub(2,1)==1
+
+def test_multiply():
+    assert obj.multiply(2,2)==4
